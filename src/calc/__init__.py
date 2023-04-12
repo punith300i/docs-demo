113375a65ea491420f05333e1bb04ee240d4dc9d
P MURALI KRISHNA <murali@Ps-MacBook-Air.lan>
2023-04-11 22:28:34 -0700

    initial docs demo content

---
+++ src/calc/__init__.py
@@ -0,0 +1,13 @@
+# calculator/calculations.py
+
+"""Provide several sample math calculations.
+
+This module allows the user to make mathematical calculations.
+
+The module contains the following functions:
+
+- `add(a, b)` - Returns the sum of two numbers.
+- `subtract(a, b)` - Returns the difference of two numbers.
+- `multiply(a, b)` - Returns the product of two numbers.
+- `divide(a, b)` - Returns the quotient of two numbers.
+"""
